ea93fc24b21291fdf4e1bd79c924bc6393733a4a
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-06-13 04:17:42 -0700

    Added collision detection for enemies and player

--- SquareCollisionGame.py
+++ SquareCollisionGame.py
@@ -17,11 +17,25 @@ player_pos = [WIDTH/2, HEIGHT-2*player_size]
 enemy_size = 50
 enemy_pos = [random.randint(0, WIDTH-enemy_size), 0]
 
+SPEED = 5
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 game_over = False
 
 clock = pygame.time.Clock()
+
+def detect_collision(player_pos, enemy_pos):
+    p_x = player_pos[0]
+    p_y = player_pos[1]
+
+    e_x = enemy_pos[0]
+    e_y = enemy_pos[1]
+
+    if (e_x >= p_x and e_x < (p_x + player_size)) or (p_x >= e_x and p_x < (e_x + enemy_size)):
+        if (e_y >= p_y and e_y < (p_y + player_size)) or (p_y >= e_y and p_y < (e_y + enemy_size)):
+            return True
+    return False
 
 while not game_over:
 
@@ -45,11 +59,17 @@ while not game_over:
             player_pos = [x, y]
     
     screen.fill(BACKGROUND_COLOR)
+
+    #updates position of enemy
     if enemy_pos[1] >= 0 and enemy_pos[1] < HEIGHT:
-        enemy_pos[1] += 5
+        enemy_pos[1] += SPEED
     else:
         enemy_pos[0] = random.randint(0, WIDTH-enemy_size)
         enemy_pos[1] = 0
+
+    #checks for collision
+    if detect_collision(player_pos, enemy_pos):
+        game_over = True
 
     pygame.draw.rect(screen, RED, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
     pygame.draw.rect(screen, BLUE, (player_pos[0], player_pos[1], player_size, player_size))
