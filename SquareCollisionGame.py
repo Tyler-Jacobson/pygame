9245959a6b0955260a0cfe3ea4868dcd3a22a6d1
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-06-13 04:47:53 -0700

    Added multiple enemies falling at once, and cleaned up code

--- SquareCollisionGame.py
+++ SquareCollisionGame.py
@@ -16,6 +16,7 @@ player_pos = [WIDTH/2, HEIGHT-2*player_size]
 
 enemy_size = 50
 enemy_pos = [random.randint(0, WIDTH-enemy_size), 0]
+enemy_list = [enemy_pos]
 
 SPEED = 5
 
@@ -24,6 +25,33 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 game_over = False
 
 clock = pygame.time.Clock()
+
+def drop_enemies(enemy_list):
+    delay = random.random()
+    if len(enemy_list) < 10 and delay < 0.05:
+        x_pos = random.randint(0, WIDTH - enemy_size)
+        y_pos = 0
+        enemy_list.append([x_pos, y_pos])
+
+def draw_enemies(enemy_list):
+    for enemy_pos in enemy_list:
+        pygame.draw.rect(screen, RED, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
+
+def update_enemy_positions(enemy_list):
+    for idx,enemy_pos in enumerate(enemy_list):
+        if enemy_pos[1] >= 0 and enemy_pos[1] < HEIGHT:
+            enemy_pos[1] += SPEED
+        
+        else:
+            enemy_list.pop(idx)
+
+def collision_check(enemy_list, player_pos):
+    for enemy_pos in enemy_list:
+        if detect_collision(enemy_pos, player_pos):
+            return True
+    return False
+
+
 
 def detect_collision(player_pos, enemy_pos):
     p_x = player_pos[0]
@@ -61,17 +89,20 @@ while not game_over:
     screen.fill(BACKGROUND_COLOR)
 
     #updates position of enemy
-    if enemy_pos[1] >= 0 and enemy_pos[1] < HEIGHT:
-        enemy_pos[1] += SPEED
-    else:
-        enemy_pos[0] = random.randint(0, WIDTH-enemy_size)
-        enemy_pos[1] = 0
 
     #checks for collision
     if detect_collision(player_pos, enemy_pos):
         game_over = True
 
-    pygame.draw.rect(screen, RED, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
+    drop_enemies(enemy_list)
+    update_enemy_positions(enemy_list)
+
+    if collision_check(enemy_list, player_pos):
+        game_over = True
+
+    draw_enemies(enemy_list)
+    
+
     pygame.draw.rect(screen, BLUE, (player_pos[0], player_pos[1], player_size, player_size))
 
     clock.tick(60)
