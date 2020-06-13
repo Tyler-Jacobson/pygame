d06ebb44e795dc97aea76ad59e9a041966ef234d
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-06-13 03:39:03 -0700

    Added falling enemy squares

--- SquareCollisionGame.py
+++ SquareCollisionGame.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 import sys
 
 pygame.init()
@@ -7,14 +8,20 @@ WIDTH = 800
 HEIGHT = 600
 
 BLUE = (0,0,255)
-BLACK = (0,0,0)
+RED = (255,0,0)
+BACKGROUND_COLOR = (0,0,0)
 
-player_pos = [400, 300]
 player_size = 50
+player_pos = [WIDTH/2, HEIGHT-2*player_size]
+
+enemy_size = 50
+enemy_pos = [random.randint(0, WIDTH-enemy_size), 0]
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 game_over = False
+
+clock = pygame.time.Clock()
 
 while not game_over:
 
@@ -37,8 +44,17 @@ while not game_over:
             
             player_pos = [x, y]
     
-    screen.fill(BLACK)
+    screen.fill(BACKGROUND_COLOR)
+    if enemy_pos[1] >= 0 and enemy_pos[1] < HEIGHT:
+        enemy_pos[1] += 5
+    else:
+        enemy_pos[0] = random.randint(0, WIDTH-enemy_size)
+        enemy_pos[1] = 0
+
+    pygame.draw.rect(screen, RED, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
     pygame.draw.rect(screen, BLUE, (player_pos[0], player_pos[1], player_size, player_size))
+
+    clock.tick(60)
 
     pygame.display.update()
 
