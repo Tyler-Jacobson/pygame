d542c9d3a0567adc8af4522707d8140817d63f2f
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-06-14 18:02:19 -0700

    Created AggroRange.py and added player movement, enemy aggro range, and enemy movement

---
+++ AggroRange.py
@@ -0,0 +1,86 @@
+import pygame
+import random
+import sys
+
+WIDTH = 800
+HEIGHT = 600
+
+player_x = 400
+player_y = 300
+player_size = 20
+
+enemy_x = 200
+enemy_y = 200
+enemy_size = 20
+aggro_range = 200
+
+PLAYER_COLOR = (0,0,255)
+ENEMY_COLOR = (255,0,0)
+AGGRO_RANGE_COLOR = (0,255,0)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+game_over = False
+
+clock = pygame.time.Clock()
+
+enemy_list = [(enemy_x, enemy_y)]
+
+def in_aggro_range(player_x, player_y, aggro_box_x, aggro_box_y, aggro_box_size):
+    if player_x >= aggro_box_x and player_x <= aggro_box_x + aggro_box_size:
+        if player_y >= aggro_box_y and player_y <= aggro_box_y + aggro_box_size:
+            return True
+    return False
+
+
+
+while not game_over:
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            game_over = True
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        player_x -= 3
+
+    if keys[pygame.K_d]:
+        player_x += 3
+
+    if keys[pygame.K_w]:
+        player_y -= 3
+    
+    if keys[pygame.K_s]:
+        player_y += 3
+
+    aggro_box_x = enemy_x - (aggro_range / 2)
+    aggro_box_y = enemy_y - (aggro_range / 2)
+    aggro_box_size = enemy_size + aggro_range
+
+    if in_aggro_range(player_x, player_y, aggro_box_x, aggro_box_y, aggro_box_size):
+        if player_x > enemy_x:
+            enemy_x += 1
+        if player_x < enemy_x:
+            enemy_x -= 1
+        if player_y > enemy_y:
+            enemy_y += 1
+        if player_y < enemy_y:
+            enemy_y -= 1
+
+
+
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, (AGGRO_RANGE_COLOR), (aggro_box_x, aggro_box_y, aggro_box_size, aggro_box_size))
+    pygame.draw.rect(screen, (ENEMY_COLOR), (enemy_x, enemy_y, enemy_size, enemy_size))
+
+    pygame.draw.rect(screen, (PLAYER_COLOR), (player_x, player_y, player_size, player_size))
+    
+
+    pygame.display.update()
+
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
+
